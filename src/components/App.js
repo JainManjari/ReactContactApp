c8c1bfaf39ddb316bdb80dd5313c262601661d56
Manjari Jain <manjjain@paypal.com>
2024-05-25 18:30:40 +0530

    useffect and localstorage for putting latest contact into the list

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, {id:contacts.length,...contact}]);
   };
+
+  useEffect(() => {
+    let retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrievedContacts) {
+      setContacts(retrievedContacts);
+    }
+  }, []); // only rendering it once
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]); // updating local storage every time a new contact is added
 
   return (
     <div className="ui container">
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,7 +3,6 @@ import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
   const { contacts } = props;
-  console.log("conatct list ", contacts);
 
   const contactList = contacts.map((contact) => {
     return <ContactCard contact={contact} key={contact.id}/>;
